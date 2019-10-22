365f6e80fe210d26ed447ed4beef660d982f12d6
yanguowei <yanguow@133.cn>
2019-10-22 15:16:47 +0800

    http

--- src/main/java/com/netty/http/HttpHelloWorldServer.java
+++ src/main/java/com/netty/http/HttpHelloWorldServer.java
@@ -2,10 +2,11 @@
 package com.netty.http;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.*;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslContext;
